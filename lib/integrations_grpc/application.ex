db8205cdaaa45aa5053c0fe4e35b8e3507fac17f
Gao Lu <glu@tigerconnect.com>
2023-04-18 11:59:00 +0800

    add grpc

--- lib/integrations_grpc/application.ex
+++ lib/integrations_grpc/application.ex
@@ -1,21 +1,21 @@
-# defmodule IntegrationsGrpc.Application do
-#   # See https://hexdocs.pm/elixir/Application.html
-#   # for more information on OTP Applications
-#   @moduledoc false
+defmodule IntegrationsGrpc.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
 
-#   use Application
+  use Application
 
-#   def start(_type, _args) do
-#     children = [
-#       # Starts a worker by calling: IntegrationsGrpc.Worker.start_link(arg)
-#       # {IntegrationsGrpc.Worker, arg}
-#       # {GRPC.Server.Supervisor, {Helloworld.Endpoint, 50_051}}
-#       {GRPC.Server.Supervisor, endpoint: Helloworld.Endpoint, port: 50051, start_server: true}
-#     ]
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: IntegrationsGrpc.Worker.start_link(arg)
+      # {IntegrationsGrpc.Worker, arg}
+      {GRPC.Server.Supervisor, {Helloworld.Endpoint, 50_051}}
+      # {GRPC.Server.Supervisor, endpoint: Helloworld.Endpoint, port: 50051, start_server: true}
+    ]
 
-#     # See https://hexdocs.pm/elixir/Supervisor.html
-#     # for other strategies and supported options
-#     opts = [strategy: :one_for_one, name: IntegrationsGrpc.Supervisor]
-#     Supervisor.start_link(children, opts)
-#   end
-# end
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: IntegrationsGrpc.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
